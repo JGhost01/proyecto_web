171f3148a232740d5eaf6df7cf323f97ffe412c9
JGhost01 <yossua1156@gmail.com>
2020-09-10 00:42:41 -0400

    update

--- Airticket/Airticket/settings.py
+++ Airticket/Airticket/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'registro.apps.RegistroConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
